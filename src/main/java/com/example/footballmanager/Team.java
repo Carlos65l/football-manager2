a981a5a27fe442fd7052492cc98508b04e9c5a13
Carlos Slaiwa <cslaiwa@tesla.com>
2024-12-16 09:28:49 +0100

    JoinColumn für league_id hinzugefügt

--- src/main/java/com/example/footballmanager/Team.java
+++ src/main/java/com/example/footballmanager/Team.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -16,6 +17,7 @@ public class Team {
     private String name;
 
     @ManyToOne //viele teams zu einer liga 
+    @JoinColumn(name = "league_id") //  Fremdschlüssel
     private League league;  // Liga direkt verknüpft
 
     // Getter und Setter
